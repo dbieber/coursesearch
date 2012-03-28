98c0ab2432a33e9f556a849e962d3c25f4511468
David Bieber <dbieber@princeton.edu>
2012-03-27 23:14:14 -0400

    SuperSearch at end

--- src/ice.js
+++ src/ice.js
@@ -5,10 +5,10 @@ CS.Ext = CS.window.Ext;
 CS.search = CS.Ext.getDom("search");
 CS.toolbar = CS.search.getElementsByClassName("x-toolbar")[0];
 CS.searchbutton = CS.toolbar.getElementsByTagName("button")[0];
-CS.searchbutton.textContent = "SuperSearch";
 CS.searchelement = CS.Ext.get(CS.searchbutton.id);
 function search(evt, el, o) {
   evt.stopPropagation();
   alert("Personal Search!");
 }
 CS.searchelement.addListener("click", search);
+CS.searchbutton.textContent = "SuperSearch";
