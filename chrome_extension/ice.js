e6d591e189d111eb33cc2d6e8fd1da8dbeafcb35
David Bieber <dbieber@princeton.edu>
2012-03-28 01:38:42 -0400

    supersearch and http requests are working :D

--- chrome_extension/ice.js
+++ chrome_extension/ice.js
@@ -1,19 +1,19 @@
 function setup() {
     CS = {};
     CS.search = Ext.getDom("search");
+    if (CS.search == null) return; // wrong frame
     CS.toolbar = CS.search.getElementsByClassName("x-toolbar")[0];
     CS.searchbutton = CS.toolbar.getElementsByTagName("button")[0];
     CS.searchelement = Ext.get(CS.searchbutton.id);
     function search(evt, el, o) {
 	evt.stopPropagation();
 	Ext.Ajax.request({
-	    url: 'http://google.com',
+	    url: 'http://requestb.in/1hot2sa1',
 	    params: {
 		id: 1
 	    },
 	    success: function(response){
 		var text = response.responseText;
-		alert(text);
 	    }
 	});
     }
