20ddd870985b25cc4166231300d60ed3c10b5706
David Bieber <dbieber@princeton.edu>
2012-03-29 19:37:09 -0400

    temp server stuff. we can remove it. and ice changes

--- chrome_extension/ice.js
+++ chrome_extension/ice.js
@@ -3,14 +3,16 @@ function setup() {
     CS.search = Ext.getDom("search");
     if (CS.search == null) return; // wrong frame
     CS.toolbar = CS.search.getElementsByClassName("x-toolbar")[0];
+    CS.searchfield = CS.toolbar.getElementsByTagName("input")[0];
     CS.searchbutton = CS.toolbar.getElementsByTagName("button")[0];
     CS.searchelement = Ext.get(CS.searchbutton.id);
     function search(evt, el, o) {
 	evt.stopPropagation();
 	Ext.Ajax.request({
-	    url: 'http://requestb.in/1hot2sa1',
+	    url: 'http://localhost:8000',
 	    params: {
-		id: 1
+		id: 1,
+		query: CS.searchfield.textContent
 	    },
 	    success: function(response){
 		var text = response.responseText;
