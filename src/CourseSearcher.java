bc7f7a0e5450c4e4848f1a55179bb805ea71e878
Abbi Ward <abigail.ward9@gmail.com>
2012-03-29 22:53:06 -0400

    CourseSearcher better -- uncomment the index line if you havent created the index yet

--- src/CourseSearcher.java
+++ src/CourseSearcher.java
@@ -16,62 +16,61 @@ import org.apache.lucene.util.Version;
 
 
 public class CourseSearcher {
-	private FSDirectory index;
-	private IndexSearcher searcher;
-	private StandardAnalyzer analyzer;
-	
-	
-	public CourseSearcher(String indexDir) throws CorruptIndexException, IOException {
-		index = FSDirectory.open(new File(indexDir));
-		IndexReader reader = IndexReader.open(index);
-		searcher = new IndexSearcher(reader);
-		analyzer = new StandardAnalyzer(Version.LUCENE_35);
-	}
-	
-	
-	/**
-	 * @param querystr
-	 */
-	public void search(String querystr) throws ParseException, IOException {
-		Query q = new QueryParser(Version.LUCENE_35, CourseDetails.TITLE , analyzer).parse(querystr);
-		int hitsPerPage = 10;
-		TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage, true);
-	    searcher.search(q, collector);
-	    ScoreDoc[] hits = collector.topDocs().scoreDocs;
-	    
-	    System.out.println("Found " + hits.length + " hits.");
-	    
-	    for(int i = 0; i < hits.length; ++i) {
-	      int docId = hits[i].doc;
-	      Document d = searcher.doc(docId);
-	      System.out.println((i + 1) + ". " + d.get(CourseDetails.COURSE));
-	    }
-	
-	}
-	
-	public void closeSearcher() throws IOException {
-		searcher.close();
-	}
-	
-	/**
-	 * @param args
-	 * @throws IOException 
-	 * @throws ParseException 
-	 * @throws ClassNotFoundException 
-	 */
-	public static void main(String[] args) throws IOException, ParseException, ClassNotFoundException {
-		String filename = "coursedata";
-		String indexDir = "testIndex";
-		RegistrarData rd = new RegistrarData();
-		rd.load(filename);
-		System.out.println("made it here!");
-		CourseIndexer indexer = new CourseIndexer(rd, indexDir);
-		CourseSearcher mysearch = new CourseSearcher(indexDir);
-		
-		mysearch.search("MUS 106");
-		
-		mysearch.search("algebra");
-		
-		mysearch.closeSearcher();
-	}
+    private FSDirectory index;
+    private IndexSearcher searcher;
+    private StandardAnalyzer analyzer;
+
+
+    public CourseSearcher(String indexDir) throws CorruptIndexException, IOException {
+        index = FSDirectory.open(new File(indexDir));
+        IndexReader reader = IndexReader.open(index);
+        searcher = new IndexSearcher(reader);
+        analyzer = new StandardAnalyzer(Version.LUCENE_35);
+    }
+
+
+    /**
+     * @param querystr
+     */
+    public void search(String querystr) throws ParseException, IOException {
+        Query q = new QueryParser(Version.LUCENE_35, CourseDetails.TITLE, analyzer).parse(querystr);
+        int hitsPerPage = 10;
+        TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage, true);
+        searcher.search(q, collector);
+        ScoreDoc[] hits = collector.topDocs().scoreDocs;
+
+        System.out.println("Found " + hits.length + " hits.");
+
+        for(int i = 0; i < hits.length; ++i) {
+            int docId = hits[i].doc;
+            Document d = searcher.doc(docId);
+            System.out.println((i + 1) + ". " + d.get(CourseDetails.COURSE));
+        }
+    }
+
+    public void closeSearcher() throws IOException {
+        searcher.close();
+    }
+
+    /**
+     * @param args
+     * @throws IOException 
+     * @throws ParseException 
+     * @throws ClassNotFoundException 
+     */
+    public static void main(String[] args) throws IOException, ParseException, ClassNotFoundException {
+        String filename = "coursedata";
+        String indexDir = "testIndex";
+        RegistrarData rd = new RegistrarData();
+        rd.load(filename);
+        //System.out.println("made it here!");
+        //CourseIndexer indexer = new CourseIndexer(rd, indexDir);
+        CourseSearcher mysearch = new CourseSearcher(indexDir);
+
+        mysearch.search("music course:mus");
+        mysearch.search("course:cos time:W");
+        //mysearch.search("linear algebra");
+
+        mysearch.closeSearcher();
+    }
 }
